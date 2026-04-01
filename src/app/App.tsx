08438d01f5c6b623b685fa57226e2692e26d29d3
sivakova.ma <sivakova.ma@plinor.ru>
2026-04-01 13:48:59 +0300

    quickfix: habit loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,22 @@
 import { HashRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import { ThemeProvider } from "@/shared/ui/theme";
+import { useAuthStore } from "@/entities/auth/model/store";
+import { useHabitsStore } from "@/entities/habit/model/store";
+import { useEffect } from "react";
 
 function App() {
+    const userId = useAuthStore((state) => state.activeUserId);
+    const loadHabitsForUser = useHabitsStore(
+        (state) => state.loadHabitsForUser,
+    );
+
+    useEffect(() => {
+        if (userId) {
+            loadHabitsForUser(userId);
+        }
+    }, [userId]);
+
     return (
         <HashRouter>
             <ThemeProvider>
